316bf233af164b93ff7d3003b97d9577552b8077
Sagar-333 <sagarkhanna030303@gmail.com>
2023-09-07 12:11:38 +0530

    Profile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
-      <div className='lg: flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24]'>
+      <div className='lg: flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]'>
         <input type='text' placeholder='Search for Campaigns' className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none'/>
         <div className='w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer'>
           <img src={search} alt='search' className='w-[15px] h-[15px] object-contain'/>
@@ -21,17 +21,21 @@ const Navbar = () => {
         </div>
       </div>
       <div className='sm:flex hidden flex-row justify-end gap-4'>
-      <CustomButton
-        btnType="button"
-        title={address ? 'Create a Campaign' : 'Connect' }
-        styles = {address ? 'bg[#1dc071]' : 'bg-[#8c6dfd]'}
-        handleClick={() => {
-          if(address) navigate('create campaign')
-          else 'connect()'
-        }
+        <CustomButton
+          btnType="button"
+          title={address ? 'Create a Campaign' : 'Connect' }
+          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+          handleClick={() => {
+            if(address) navigate('create campaign')
+            else 'connect()'
+          }}
+        />
+        <Link to='/profile'>
+          <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+          <img src={thirdweb} alt='user' className='w[60%] h-[60%] object-contain'/>
+          </div>
+        </Link>
 
-        }
-      />
       </div>
     </div>
   )
